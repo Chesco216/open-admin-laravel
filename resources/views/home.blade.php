0df4ee1e577e24a08f3995129310b0ca201aa150
Chesco216 <franchescofv198@gmail.com>
2024-12-18 16:32:55 -0400

    pipipi

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body>
+    <div class='flex flex-col items-center justify-center w-full'>
+        <h1 class='w-full text-center font-bold text-3xl bg-slate-700 text-white py-4'>Dasboard pipipi</h1>
+        <div class='w-full h-full bg-green-300'>
+
+        </div>
+    </div>
+</body>
+</html>
